35afb0957af552766da29fbaf8340b5796ef801c
samy <samy.t.khelifa@gmail.com>
2019-07-11 22:44:03 +0200

    initial setup before  explicit intent implementation         modified:   app/src/main/AndroidManifest.xml         modified:   app/src/main/java/com/example/explicitintent/MainActivity.java         new file:   app/src/main/java/com/example/explicitintent/SecondActivity.java         modified:   app/src/main/res/layout/activity_main.xml         new file:   app/src/main/res/layout/activity_second.xml         modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.explicitintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private Button submitBtn;
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        submitBtn = findViewById(R.id.submitBtn);
+        nameEditText = findViewById(R.id.nameEditText);
+        submitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                moveToSecondActivity_FirstWay();
+                // Uncomment it to test the method.
+              //  moveToSecondActivity_SecondWay();
+            }
+
+            private void moveToSecondActivity_FirstWay() {
+
+            }
+
+            private void moveToSecondActivity_SecondWay() {
+            }
+        });
+
     }
 }
